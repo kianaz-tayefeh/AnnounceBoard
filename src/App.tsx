42909a3467acd7c2f384dc3fda1d4ce42ee6f53f
kianaz <kianaztayefeh@gmail.com>
2023-11-19 13:19:24 +0100

    test hsk

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { AnnounceDetail } from "src/components/pages/AnnounceDetail";
 
 export default function App() {
   const x = 1;
-  const x = 2;
+  const x = 3;
   return (
     <MantineProvider theme={theme}>
       <AnnounceDetail />
